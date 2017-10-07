1a8a6e333d48dab90ac39e04334583dbff8a44c0
“Vilde <vilde@vevatne.no>
2017-10-07 11:16:40 -0700

    Add titles to pin components

--- client/src/components/pin.js
+++ client/src/components/pin.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 
-const Pin = ({ data: { images } }) => {
+const Pin = ({ data: { images, title } }) => {
 	return (
 		<div>
-			<img src={images["136x136"].url} />
+			<img src={images["236x"].url} />
+			<p>{title}</p>
 		</div>
 	);
 };
