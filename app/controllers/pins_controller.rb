5ea4b251427e5a39c7e4fdc5450bf4e189d68002
“Vilde <vilde@vevatne.no>
2017-10-09 14:02:19 -0700

    Add paginate method

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
+require 'will_paginate/array'
+
 class PinsController < ApplicationController
-	require 'will_paginate/array'
   def index
   	@pins = JSON.parse(File.read("app/assets/javascripts/pins_formatted.json"))
   	@pins.paginate(page: params[:page], per_page: 10) 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pins/index'
+  get 'pins', to: 'pins#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
